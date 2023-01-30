1aef44c563cf3618f4798e1c68cb4fde22e4c4aa
Lauren Thomson <laurenthomson@laurens-mbp-3.middlebury.edu>
2023-01-30 11:27:17 -0500

    weekly assignment 3, assignment 8, index updates

---
+++ 2023-01-26-import-data-notes.R
@@ -0,0 +1,78 @@
+#### Lecture 8: Loading in Data
+#### Jan 26 2023
+#### LHT
+
+#Functions to create and save dataset
+###write.table(x=varName, file="outputFileName.csv",  header=True, sep = ",")
+
+
+###Functions to read in dataset
+## write.table(file="path/to/data.csv", header=True, sep = ",")
+##read.csv(file="data.csv", header=T)
+
+### Use RDS object when only working in R
+## saveRDS(my_data, file="FileName.RDS")
+## readRDS("FileName")
+## p <- readRDS("FileName".RDS)
+
+### Long vs Widedata formats 
+### wide format = contains values that do not repeat in the ID colunm 
+### long format = contains values that DO repeat in the ID column
+
+library(tidyverse)
+glimpse(billboard)
+
+b1<- billboard %>% 
+  pivot_longer(
+    cols = starts_with("wk"), #specificy which columns you want to make 'longer'
+    names_to = "Week", # name of new column which will contain the header names
+    values_to = "Rank", # name of new column containing values
+    values_drop_na = TRUE #removes rows withs nas
+  )
+
+View(b1)
+
+
+#pivot_wider
+## best for making occupancy matrix
+
+glimpse(fish_encounters)
+view(fish_encounters)
+
+fish_encounters %>%
+  pivot_wider(names_from = station, #which column we want to become multiple columns
+              values_from = seen, # which column contains values for new column cells
+              values_fill=0) # fills nas with 0s 
+
+
+##### Dryad: makes research data freely reusable, citable, and discoverable.
+
+##read.table()
+dryadData <- read.table(file="veysey_babbitt_amphibians.csv", header=T, sep = ",")
+glimpse(dryadData)
+head(dryadData)
+table(dryadData$species) # 2 species and 66 of each
+summary(dryadData$mean.hydro) # min mean max med 1st/3rd quartile
+
+str(dryadData$species)
+dryadData$species<-factor(dryadData$species, labels=c("Spotted Salamander", "Wood Frog")) #creating 'labels' to use for the plot
+str(dryadData$species)
+
+class(dryadData$treatment)
+dryadData$treatment <- factor(dryadData$treatment, 
+                              levels=c("Reference",
+                                       "100m", "30m"))
+library(paletteer)
+p<- ggplot(data=dryadData, 
+           aes(x=interaction(wetland, treatment), 
+               y=count.total.adults, fill=factor(year))) + geom_bar(position="dodge", stat="identity", color="white") + #made white outlines 
+  ylab("Number of breeding adults") +
+  xlab("") +
+  scale_y_continuous(breaks = c(0,100,200,300,400,500)) +
+  scale_x_discrete(labels=c("30 (Ref)", "124 (Ref)", "141 (Ref)", "25 (100m)","39 (100m)","55 (100m)","129 (100m)", "7 (30m)","19 (30m)","20 (30m)","59 (30m)")) +
+  facet_wrap(~species, nrow=2, strip.position="right") +
+  theme_few() + scale_fill_hue(l=40)+ #changed fill of bar graph
+  theme(panel.background = element_rect(fill = 'black', color = 'black'), legend.position="top",  legend.title= element_blank(), axis.title.y = element_text(size=12, face="bold", colour = "black"), strip.text.y = element_text(size = 10, face="bold", colour = "black")) + # changed baclground
+  guides(fill=guide_legend(nrow=1,byrow=TRUE)) 
+
+print(p)
